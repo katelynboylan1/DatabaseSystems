41f84c1f26bb2e1055c6f5280a665764bb603cd3
katelynboylan1 <katelyn.boylan1@marist.edu>
2017-02-03 13:43:59 -0500

    Add files via upload

---
+++ KatelynBoylan_2.7.17_Lab3.sql
@@ -0,0 +1,53 @@
+﻿--Katelyn Boylan
+--Lab 3
+
+-- Number 1
+SELECT ordNumber, totalUSD
+from Orders;
+
+--Number 2
+SELECT name, city
+from Agents
+where name='Smith'; 
+
+--Number 3
+SELECT pid, name, quantity, priceUSD
+from Products
+where quantity > 200100;
+
+--Number 4
+SELECT name, city
+from Customers
+where city = 'Duluth';
+
+--Number 5
+SELECT name, city
+from Agents
+where city <> 'New York'
+and city <> 'Duluth';
+
+--Number 6
+SELECT priceUSD, name 
+from products
+where priceUSD >= 1.00
+and city <> 'Duluth'
+and city <> 'Dallas';
+
+--Number 7
+SELECT *
+from Orders
+where month = 'Feb' OR month = 'Apr';
+
+--Number 8
+SELECT *
+from Orders
+where month = 'Feb' 
+AND totalUSD >= 600;
+
+--Number 9
+SELECT *
+from Orders
+where cid = 'c005';
+
+
+
